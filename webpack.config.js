537e2a3e35c669a97caab4f6e120c0e3d5bc95cb
夏奈 <shuaiji.tsj@alipay.com>
2017-08-30 00:23:10 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var webpackConfig = {
+  entry: {
+    'app': './src/app.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.htm',
+      template: 'src/index.htm'
+    }),
+
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, './src/vendor'),
+        to: path.resolve(__dirname, './dist/vendor'),
+        ignore: ['.*']
+      }
+    ])
+  ]
+}
+
+module.exports = webpackConfig
