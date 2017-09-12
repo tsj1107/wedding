1f7cbfe8a9013229ebed112e808db03a5c60694e
夏奈 <shuaiji.tsj@alipay.com>
2017-09-13 03:15:35 +0800

    map & invite

--- src/app.js
+++ src/app.js
@@ -9,6 +9,15 @@ const scenes = [
       $('.scene:nth-child(1)')
         .show()
         .animate('flipInY', 600, 'ease')
+      $('#bg .left').animate({
+        left: '-10%'
+      }, 600, 'ease')
+      $('#bg .right').animate({
+        right: '-10%'
+      }, 600, 'ease')
+      $('#bg .bottom').animate({
+        bottom: 0
+      }, 600, 'ease')
     },
     hide: function() {
       $('.scene:nth-child(1)').hide()
@@ -16,68 +25,75 @@ const scenes = [
   },
   {
     show: function() {
-      $('.scene:nth-child(2) .chloe').animate({
-        bottom: 0
-      }, 600, 'ease')
-      $('.scene:nth-child(2) .pop1')
+      $('.scene:nth-child(2) .chloe')
         .show()
         .animate('flipInX', 600, 'ease')
-    },
-    hide: function() {
-      $('.scene:nth-child(2) .pop1').hide()
-      $('.scene:nth-child(2) .chloe').animate({
+      $('.scene:nth-child(2) .pop')
+        .show()
+        .animate('fadeIn', 2000, 'ease')
+      $('#bg .left').animate({
+        left: '-100%'
+      }, 600, 'ease')
+      $('#bg .right').animate({
+        right: '-100%'
+      }, 600, 'ease')
+      $('#bg .bottom').animate({
         bottom: '-100%'
       }, 600, 'ease')
+    },
+    hide: function() {
+      $('.scene:nth-child(2) .chloe')
+        .hide()
+      $('.scene:nth-child(2) .pop')
+        .hide()
     }
   },
   {
     show: function() {
-      $('.scene:nth-child(3) .quark').animate({
-        bottom: 0
-      }, 600, 'ease')
-      $('.scene:nth-child(3) .pop2')
+      $('.scene:nth-child(3) .quark')
         .show()
         .animate('flipInX', 600, 'ease')
+      $('.scene:nth-child(3) .pop')
+        .show()
+        .animate('fadeIn', 2000, 'ease')
     },
     hide: function() {
-      $('.scene:nth-child(3) .pop2').hide()
-      $('.scene:nth-child(3) .quark').animate({
-        bottom: '-100%'
-      }, 600, 'ease')
+      $('.scene:nth-child(3) .quark').hide()
+      $('.scene:nth-child(3) .pop').hide()
     }
   },
   {
     show: function() {
-      $('.scene:nth-child(4) .group').animate({
-        bottom: 0
-      }, 600, 'ease')
-      $('.scene:nth-child(4) .pop3')
+      $('.scene:nth-child(4) .group')
         .show()
         .animate('flipInX', 600, 'ease')
-    },
-    hide: function() {
-      $('.scene:nth-child(4) .pop3').hide()
-      $('.scene:nth-child(4) .group').animate({
-        bottom: '-100%'
-      }, 600, 'ease')
-    }
-  },
-  {
-    show: function() {
-      $('.scene:nth-child(5) .pop4')
+      $('.scene:nth-child(4) .pop')
         .show()
-        .animate('rotateIn', 600, 'ease')
+        .animate('fadeIn', 2000, 'ease')
     },
     hide: function() {
-      $('.scene:nth-child(5) .pop4').hide()
+      $('.scene:nth-child(4) .group').hide()
+      $('.scene:nth-child(4) .pop').hide()
     }
   },
   {
     show: function() {
-      $('.scene:nth-child(6)').show()
+      $('.scene:nth-child(5) .pop')
+        .show()
+        .animate('fadeIn', 2000, 'ease')
     },
     hide: function() {
-      $('.scene:nth-child(6)').hide()
+      $('.scene:nth-child(5) .pop').hide()
+    }
+  },
+  {
+    show: function() {
+      $('.scene:nth-child(6) .white')
+        .show()
+        .animate('zoomIn', 600, 'ease')
+    },
+    hide: function() {
+      $('.scene:nth-child(6) .white').hide()
     }
   }
 ]
